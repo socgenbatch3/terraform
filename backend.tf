4d88081e420e79bf7049e5142686484b69e054d5
Ravi <ravi@vytec.com>
2024-09-06 09:21:27 +0530

    updated

---
+++ backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstateRG"  
+    storage_account_name = "tfstatestgacc04092024"                   
+    container_name       = "tfstate"                     
+    key                  = "terraform.tfstate" 
+    client_id       = "clientid"
+    client_secret   = "clientsecret"
+    tenant_id       = "tenantid"
+    subscription_id = "subscriptionid"
+  }
+}
